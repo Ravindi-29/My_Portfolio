df360e79893411d774fda1e3fb6e97e00206225a
Ravindi Ranthilini Lanka Geeganage <rranthilini29@gmail.com>
2026-06-11 14:46:23 +0530

    Replace anchor tags with Link components

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,9 +11,9 @@ export default function Home() {
           <h1>I'm <span className="highlight">Ravindi Ranthilini</span></h1>
           <p className="description">I am a Information Technology student at the Institute of Technology, University of Moratuwa. I am a passionate UI/UX designer with a strong background in creating intuitive and engaging user experiences. I create user interfaces that look good, are easy to use, and work well. I pay close attention to details and focus on the needs of users to build digital products that are helpful, enjoyable, and effective.</p>
           <div className="buttons">
-              <a href="/contacts" className="btn primary">Hire Me</a>
-              <a href="/about" className="btn secondary">Experience</a>
-              <a href="/projects" className="btn secondary">View Projects</a>
+              <Link to="/contacts" className="btn primary">Hire Me</a>
+              <Link to="/about" className="btn secondary">Experience</a>
+              <Link to="/projects" className="btn secondary">View Projects</a>
             
           </div>
        <div className="home right">
